fd552d74c0709fe46c8c5b8671226808373a01db
jusong23 <jusong23@naver.com>
2022-07-16 14:55:05 +0900

    Initial Commit

---
+++ RisingCamp_2nd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp_2nd
+//
+//  Created by 이주송 on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
