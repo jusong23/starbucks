bb01dc3f193f1f9fdc3611b182efcaf61a0cc7fc
jusong23 <jusong23@naver.com>
2022-07-17 19:57:46 +0900

    Image View

--- RisingCamp_2nd/ViewController.swift
+++ RisingCamp_2nd/ViewController.swift
@@ -9,22 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var scroll: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var wellComeMessage: UIStackView!
     @IBOutlet weak var stampSet: UIStackView!
     @IBOutlet weak var WhatsnewAndCouponVIew: UIView!
-    
+    @IBOutlet weak var star100: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.scroll.delegate = self
+        self.WhatsnewAndCouponVIew.layer.borderWidth = 0
+        self.WhatsnewAndCouponVIew.layer.shadowColor = UIColor.black.cgColor
+        self.WhatsnewAndCouponVIew.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.WhatsnewAndCouponVIew.layer.shadowOpacity = 0.2
+        
+ 
     }
+
     
 }
 
 extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
         print(self.scroll.contentOffset.y)
         
         let nowScrollOffset = self.scroll.contentOffset.y
@@ -33,5 +42,6 @@ extension ViewController: UIScrollViewDelegate {
         self.imageView.alpha = 1 - (nowScrollOffset / imageHeight )
         self.wellComeMessage.alpha = 1 - (nowScrollOffset / imageHeight)
         self.stampSet.alpha = 1 - (nowScrollOffset / imageHeight)
+
     }
 }
