7babfb4cbf823356339c0977df3a743ad1e2236c
jusong23 <jusong23@naver.com>
2022-07-17 11:15:57 +0900

    App Switcher

--- RisingCamp_2nd/SceneDelegate.swift
+++ RisingCamp_2nd/SceneDelegate.swift
@@ -11,6 +11,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    // TODO: 1. 이미지뷰 생성
+        var imageView: UIImageView?
+
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         print(#function)
         print("[Scene] 연결중")
@@ -25,11 +29,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         print(#function)
         print("[Scene] 우리 눈에 보이는 화면에 도달했습니다.")
+        
+        // TODO: 3. 앱이 다시 활성화 상태가 되면 이미지뷰를 superview (window)에서 제거한다
+        if let imageView = imageView {
+            imageView.removeFromSuperview()
+        }
+        
     } // 우리 눈에 보이는 화면에 도달했을때 도달 이후 나오는 함수(Forground Active) Cycle.1
     
     func sceneWillResignActive(_ scene: UIScene) {
         print(#function)
         print("[Scene] 앱을 위로 띄우셨군요")
+        
+        // TODO: 2. will resign active 상태가 호출되면 imageview를 window 크기로 잡아주고 window에 추가한다
+        guard let window = window else {
+            return
+        }
+        imageView = UIImageView(frame: window.frame)
+        imageView?.image = UIImage(named: "switcher" )
+        window.addSubview(imageView!)
+        
     } // 위로 슬라이드해서 강제종료 하는 상황. (은행,스타벅스 앱 생각해보기) Cycle.2
     
     func sceneDidEnterBackground(_ scene: UIScene) {
