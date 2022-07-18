7f3e42d031532272970b9e211c3c8874435878ef
jusong23 <jusong23@naver.com>
2022-07-18 19:30:16 +0900

    Hide Navigation Bar

--- RisingCamp_2nd/AccountInfoViewController.swift
+++ RisingCamp_2nd/AccountInfoViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 
 class AccountInfoViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        self.navigationController?.navigationBar.isHidden = true
+
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+
+extension AccountInfoViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
+        if self.scrollView.contentOffset.y > 0 {
+            UIView.animate(withDuration: 2, animations: {            self.navigationItem.title = "계정정보"
+            })
+        } else {
+            UIView.animate(withDuration: 2, animations: {            self.navigationItem.title = ""
+            })
+
+        }
     }
 }
