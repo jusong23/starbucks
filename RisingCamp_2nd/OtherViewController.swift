c533aa06b11151f3f53718ef4c8762db3c6cdab3
jusong23 <jusong23@naver.com>
2022-07-18 16:37:07 +0900

    Height Test2

--- RisingCamp_2nd/OtherViewController.swift
+++ RisingCamp_2nd/OtherViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class OtherViewController: UIViewController {
 
+    @IBOutlet weak var otherView: UIView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.otherView.layer.borderWidth = 0
+        self.otherView.layer.shadowColor = UIColor.black.cgColor
+        self.otherView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.otherView.layer.shadowOpacity = 0.2
     }
+
+}
+
+extension OtherViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if self.scrollView.contentOffset.y > 0 {
+            self.navigationItem.title = "Other"
+        } else {
+            self.navigationItem.title = ""
+        }
     }
-    */
-
 }
